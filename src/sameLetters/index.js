d4be6310f401c48d70ad7f321fb9cb6e452710fd
White Nguyen <whitenguyen0909@gmail.com>
2022-07-10 11:04:30 -0600

    same letters solution with map and inner set

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,22 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+    const results = new Map();
+    words.forEach(word => {
+        word.toLowerCase();
+        const uniqueChars = new Set(word.split(""));
+        const arrayUniqueChars = [...uniqueChars].sort();
+        const stringUniqueChars = arrayUniqueChars.join("");
+        if (results.has(stringUniqueChars)) {
+            results.get(stringUniqueChars).push(word)
+        } else {
+            results.set(stringUniqueChars, [word])
+        }
+    })
 
+    return results;
+}
+const a = sameLetters(["pair", "per", "pole", "pear", "peer", "reap", "lope"]);
+console.log(a)
 module.exports = sameLetters;

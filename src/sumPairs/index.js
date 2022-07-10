6ce7fdde6f23ad9b200f19209a12eb40da5fb5b4
White Nguyen <whitenguyen0909@gmail.com>
2022-07-10 11:16:40 -0600

    sum of pairs solution with maps

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,21 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+  const numbers = new Map();
+  const solution = new Map();
+  A.forEach((element) => {
+    numbers.set(element);
+  });
+  A.forEach((element) => {
+    const diff = N - element;
+    if (diff !== element) {
+      if (numbers.has(diff)) {
+        solution.set(Math.min(element, diff), Math.max(element, diff));
+      }
+    }
+  });
+  return Array.from(solution);
+}
 
 module.exports = sumPairs;

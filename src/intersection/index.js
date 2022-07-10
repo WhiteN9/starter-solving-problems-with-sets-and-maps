90b75d337cc804436232ca159be27a0d2550ccec
White Nguyen <whitenguyen0909@gmail.com>
2022-07-10 10:25:54 -0600

    intersection solution

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,20 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    if (a.length < 1 || b.length < 1 ) {
+        return [];
+    }
+    const map = new Map();
+    const set = new Set();
+    a.forEach(element => {
+        map.set(element)
+    });
+    // console.log(map)
+    b.forEach(element => {
+        if(map.has(element)) {
+            set.add(element)
+        }
+    })
+    // console.log(set)
+    return [...set]
+}
 
 module.exports = intersection;
